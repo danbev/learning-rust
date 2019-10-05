8c5701231337252e2f59e6c2a7a20edb77927212
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-10-05 06:50:09 +0200

    src: add pattern matching to enums example

--- snippets/src/enums.rs
+++ snippets/src/enums.rs
@@ -1,24 +1,26 @@
 
-    #[derive(Debug)]
-    enum Something {
-        ONE(String),
-        TWO(String),
+#[derive(Debug)]
+enum Something {
+    ONE(String),
+    TWO(String),
+}
+
+fn func(s :Something) {
+  println!("{:?}", s);
+  match s {
+      Something::ONE(value) => println!("value: {}", value),
+      _ => println!("no value"),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Something;
+    use super::func;
+    #[test]
+    fn enums_test() {
+        let s = Something::ONE(String::from("first"));
+        func(s);
+        assert_eq!(2 + 2, 4);
     }
-
-    fn func(s :Something) {
-      println!("{:?}", s);
-    }
-
-
-    #[cfg(test)]
-    mod tests {
-        use super::Something;
-        use super::func;
-        #[test]
-        fn enums_test() {
-            let s = Something::ONE(String::from("first"));
-            func(s);
-            assert_eq!(2 + 2, 4);
-    }
-
 }
