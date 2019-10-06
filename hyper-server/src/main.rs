d3d80f56fcf8743711f08aa3141ea2d91d3ec673
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-10-06 17:24:52 +0200

    src: add basic hyper server

---
+++ hyper-server/src/main.rs
@@ -0,0 +1,35 @@
+#![deny(warnings)]
+extern crate hyper;
+extern crate pretty_env_logger;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::{self, Future};
+
+fn main() {
+    pretty_env_logger::init();
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    /*
+    let _service = service_fn_ok(move |_: Request<Body>| {
+        Response::new(Body::from("Hello World!"))
+    });
+    */
+
+    let server = Server::bind(&addr)
+        .serve(move || {
+            // This is the `Service` that will handle the connection.
+            // `service_fn_ok` is a helper to convert a function that
+            // returns a Response into a `Service`.
+            let message = String::from("bajja");
+            service_fn_ok(move |_: Request<Body>| {
+                println!("{}", message);
+                Response::new(Body::from("Hello World!"))
+            })
+        })
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    rt::run(server);
+}
