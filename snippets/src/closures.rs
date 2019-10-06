6af98a16f681477bbae34b9f6fec7c0787742e54
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-10-06 15:21:08 +0200

    src: minor cleanup of closure example

--- snippets/src/closures.rs
+++ snippets/src/closures.rs
@@ -3,8 +3,10 @@
 mod tests {
     #[test]
     fn closure_test() {
+        let s = String::from("bajja");
         let c = |name: String| -> String {
-            println!("Closure name: {}", name);
+            assert_eq!(name, "Fletch");
+            assert_eq!(s, "bajja");
             String::from("done")
         };
         let r = c(String::from("Fletch"));
