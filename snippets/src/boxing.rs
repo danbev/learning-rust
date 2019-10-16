a1733d1545682b0f84878a0ba9411863a40cf7f7
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-10-16 06:13:34 +0200

    src: add Deref example

--- snippets/src/boxing.rs
+++ snippets/src/boxing.rs
@@ -1,6 +1,27 @@
+use std::ops::Deref;
+
+// this is a tuple struct with one member
+struct B<T>(T);
+
+impl<T> Deref for B<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+
+}
+
+impl<T> B<T> {
+    fn new(x: T) -> B<T> {
+        B(x)
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn boxing_test() {
         let x = 18;
@@ -12,5 +33,8 @@ mod tests {
         assert_eq!(*z, 18);
         *z += 1;
         println!("--------->x: {}, y: {}, &y: {:p}, z: {}, &z:{:p}", x, y, y, z, z);
+
+        let i = B::new(x);
+        assert_eq!(*i, 18);
     }
 }
