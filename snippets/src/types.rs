1cc8adc6e87c6b9b9da32d6da4a7d93e124e0521
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-12-09 12:30:06 +0100

    src: update types_test to print pointers

--- snippets/src/types.rs
+++ snippets/src/types.rs
@@ -5,7 +5,9 @@ mod tests {
     fn types_test() {
         let nr: i32 = 10;
         let nr2 = &10;
-        println!("nr = {}", nr);
+        println!("nr = {} {:p}", nr, &nr);
+        println!("nr2 = {} {:p}", nr2, nr2);
         assert_eq!(nr, 10);
+        assert_eq!(nr2, &10);
     }
 }
