0f5c0fc6bbfbe222c8a2db3994cce66b5ce45742
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-09-15 18:36:40 +0200

    return prk from extract function

--- crypto/src/main.rs
+++ crypto/src/main.rs
@@ -1,5 +1,5 @@
 use sha2::{Sha256, Digest};
-use hkdf::{Hkdf, HkdfExtract};
+use hkdf::{Hkdf};
 
 fn digest_example() {
     let mut hasher = Sha256::new();
@@ -12,33 +12,29 @@ fn digest_example() {
     println!("{:x}", sha2::Sha256::digest(b"bajja"));
 }
 
-fn hkdf_extract_example() -> Hkdf<Sha256> {
-    let mut extract_ctx = HkdfExtract::<Sha256>::new(Some(b"mysalt"));
-    extract_ctx.input_ikm(b"hello");
-    extract_ctx.input_ikm(b" world");
-    let (streamed_res, hkdf) = extract_ctx.finalize();
-    println!("Expanded: {:x}", streamed_res);
+fn hkdf_extract_example<'a>(salt: &str, ikm: &str) -> (Vec<u8>, Hkdf<Sha256>) {
+    let salt_hex = hex::decode(salt).unwrap();
+    let ikm_hex = hex::decode(ikm).unwrap();
 
-    let (oneshot_res, _) = Hkdf::<Sha256>::extract(Some(b"mysalt"), b"hello world");
-    assert_eq!(streamed_res, oneshot_res);
-    hkdf
+    let (prk, hkdf) = Hkdf::<Sha256>::extract(Some(&salt_hex), &ikm_hex);
+    let s = prk.as_slice();
+    println!("oneshot_res: {:?}", prk);
+    (s.to_owned(), hkdf)
 
 }
 
-fn hkdf_expand_example(hkdf: Hkdf<Sha256>) {
-    // Our initial key matrial which we want to expand
-    //let ikm = hex::decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
-    //let salt = hex::decode("00000000000000000000000000").unwrap();
-    let info = hex::decode("f0f1f2f3f4f5f6f7f8f9").unwrap();
-
-    //let h = Hkdf::<Sha256>::new(Some(&salt[..]), &ikm);
+fn hkdf_expand_example(hkdf: Hkdf<Sha256>, prk: &Vec<u8>) {
     let mut okm = [0u8; 42];
-    hkdf.expand(&info, &mut okm).unwrap();
+    hkdf.expand(&prk, &mut okm).unwrap();
     println!("Output Key Material: {}", hex::encode(&okm[..]));
 }
 
 fn main() {
     digest_example();
-    let hkdf = hkdf_extract_example();
-    hkdf_expand_example(hkdf);
+    let salt = "00000000000000000000000000";
+    let ikm = "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b";
+
+    let (prk, hkdf) = hkdf_extract_example(&salt, &ikm);
+    println!("prk: {:?}", prk);
+    hkdf_expand_example(hkdf, &prk);
 }
