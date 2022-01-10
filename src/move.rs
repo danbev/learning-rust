9b759cea7d15f7a19c3ebab7937cd28717287d2e
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-10 13:58:26 +0100

    Add example of move with closure

--- src/move.rs
+++ src/move.rs
@@ -1,4 +1,12 @@
+fn something() {
+    println!("something...");
+}
+
 fn main() {
     let s = [0; 1024];
     let t = s;
+
+    // Note that the following is a closure, and something() is the body.
+    let closure = move || something();
+    closure();
 }
