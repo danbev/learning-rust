3b7cd2894e6da2b28fce156c2ec789a450fb05dc
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-07-01 14:47:41 +0200

    Add trailing_zeros example

---
+++ src/trailing_zeros.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    println!("trailing_zeros example");
+    println!("nr: {}", 0b1_i32.trailing_zeros());
+    println!("nr: {}", 0b10_i32.trailing_zeros());
+    println!("nr: {}", 0b11_i32.trailing_zeros());
+    println!("nr: {}", 0b100_i32.trailing_zeros());
+
+}
