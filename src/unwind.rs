25a134ce1e568abf3172b961e83e9f01709a729a
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-07-13 13:27:54 +0200

    Extend the unwind example

--- src/unwind.rs
+++ src/unwind.rs
@@ -1,18 +1,24 @@
 use std::panic;
 
-fn f() {
-    struct S {
-    }
-}
+type SomeResult<T> = std::result::Result<T, SomeError>;
+#[derive(Debug, Clone)]
+struct SomeError;
 
 fn main() {
-    let r = panic::catch_unwind(|| { println!("all is well"); });
+    let r = panic::catch_unwind(|| {
+        println!("all is well");
+    });
     println!("result.is_ok(): {}", r.is_ok());
 
-    let r = panic::catch_unwind(|| { panic!("doh"); });
+    let r = panic::catch_unwind(|| {
+        panic!("doh");
+    });
     println!("result.is_err(): {}", r.is_err());
 
-    let r = panic::catch_unwind(|| { panic!("doh"); }).unwrap_or(18);
+    let r = panic::catch_unwind(|| {
+        panic!("doh2");
+    }).unwrap_or(18);
+
     println!("unwrap_or: {}", r);
 
     let default = 101;
@@ -25,4 +31,20 @@ fn main() {
     println!("{:?}", r);
     let x = r.unwrap_or(default);
     println!("{:?}", x);
+
+    let r = something(false);
+    println!("someresult {:?}", r);
+    let r = something(true);
+    println!("someresult {:?}", r);
+}
+
+
+fn something(r#try: bool) -> SomeResult<i32> {
+    if r#try == true {
+        let nr= panic::catch_unwind(move || { 18 } ).map_err(|_| SomeError)?;
+        Ok(nr)
+    } else {
+        let err = panic::catch_unwind(move || { SomeError } ).map_err(|_| SomeError)?;
+        Err(err)
+    }
 }
