be6b38b4a770b0cab6eb92f08e459b106190724c
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-10-06 12:56:07 +0200

    Add Option::map_or example

--- src/option.rs
+++ src/option.rs
@@ -7,7 +7,33 @@ fn something_string(_: String) -> Option<String> {
 }
 
 fn main() {
-    println!("os2: {:?}", Some(18).and_then(something).and_then(something_string));
-    println!("os3: {:?}", None.and_then(something).and_then(something_string));
+    println!(
+        "os2: {:?}",
+        Some(18).and_then(something).and_then(something_string)
+    );
+    println!(
+        "os3: {:?}",
+        None.and_then(something).and_then(something_string)
+    );
 
+    let os = OptionalSomething {
+        field: Some(Prov { x: 1 }),
+    };
+
+    let b = os.field.map_or(0, |p| p.x);
+    println!("b: {}", b);
+}
+
+struct Prov {
+    x: u8,
+}
+
+struct OptionalSomething {
+    field: Option<Prov>,
+}
+
+impl OptionalSomething {
+    fn has_prov(&self) -> u8 {
+        self.field.as_ref().map_or(0, |p| p.x)
+    }
 }
