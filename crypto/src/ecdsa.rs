058ba1b216c1b2e42b76eaad7ce7da355207d5be
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-12-01 14:03:13 +0100

    Add parsing of der ecdsa format
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- crypto/src/ecdsa.rs
+++ crypto/src/ecdsa.rs
@@ -1,7 +1,9 @@
+use derp::{Der, Tag};
 use ring::{
     rand,
     signature::{self, KeyPair, ECDSA_P256_SHA256_ASN1_SIGNING},
 };
+use untrusted::Input;
 
 fn main() {
     println!("Ecdsa example");
@@ -21,7 +23,39 @@ fn main() {
     let pkcs8_doc =
         // returns a serialized PKCS#8 document
         signature::EcdsaKeyPair::generate_pkcs8(&ECDSA_P256_SHA256_ASN1_SIGNING, &rng).unwrap();
-    println!("PKCS#8: {:?}\n", pkcs8_doc.as_ref());
+    let b: &[u8] = pkcs8_doc.as_ref();
+
+    println!("As hex:");
+    for i in b {
+        print!("{:02x?}", i);
+    }
+    println!("\n");
+
+    let input = Input::from(b);
+    let (ecc_oid, alg_oid, private_key) = input
+        .read_all(derp::Error::Read, |input| {
+            println!("read_all: {:?}", input);
+            // read all of the sequence (30 hex (48 dec) is the value of the Sequence
+            // tag. So we read it.
+            let (ecc_oid, alg_oid, private_key) = derp::nested(input, Tag::Sequence, |input| {
+                println!("input: {:?}", input);
+                let version = derp::expect_tag_and_get_value(input, Tag::Integer)?;
+                println!("version: {:?}", version);
+                let (ecc_oid, alg_oid) = derp::nested(input, Tag::Sequence, |input| {
+                    let ecc_oid = derp::expect_tag_and_get_value(input, Tag::Oid)?;
+                    println!("ecc_oid value: {:?}", ecc_oid);
+                    let alg_oid = derp::expect_tag_and_get_value(input, Tag::Oid)?;
+                    println!("alg_oid value: {:?}", alg_oid);
+                    Ok((ecc_oid, alg_oid))
+                })?;
+                let private_key = derp::expect_tag_and_get_value(input, Tag::OctetString)?;
+                println!("private_key: {:?}", private_key);
+                Ok((ecc_oid, alg_oid, private_key))
+            })?;
+            Ok((ecc_oid, alg_oid, private_key))
+        })
+        .unwrap();
+    println!("ecc_oid: {:02x?}", ecc_oid.as_slice_less_safe());
 
     let bytes = pkcs8_doc.as_ref();
     let key_pair =
@@ -30,7 +64,7 @@ fn main() {
 
     const MSG: &[u8] = b"Fletch";
     let sig = key_pair.sign(&rand::SystemRandom::new(), MSG).unwrap();
-    println!("Signature: {:?}\n", &sig.as_ref());
+    //println!("Signature: {:?}\n", &sig.as_ref());
 
     let public_key = key_pair.public_key();
     println!("{:?}", public_key);
