bf3390723d33db1a605b63d5a7273520a742616b
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-03-13 11:55:31 +0100

    src: add get_function and get_function_name
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- binaryen-example/src/update.rs
+++ binaryen-example/src/update.rs
@@ -1,7 +1,10 @@
 use binaryen_sys::{
-    BinaryenModuleDispose, BinaryenModulePrint, BinaryenModuleRead, BinaryenModuleRef,
+    BinaryenExpressionPrint, BinaryenFunctionGetBody, BinaryenFunctionGetName, BinaryenFunctionRef,
+    BinaryenGetFunction, BinaryenModuleDispose, BinaryenModulePrint, BinaryenModuleRead,
+    BinaryenModuleRef,
 };
 
+use std::ffi::{CStr, CString};
 use std::fs::File;
 use std::io::Read;
 
@@ -14,11 +17,31 @@ fn read_module(filename: &str) -> BinaryenModuleRef {
     unsafe { BinaryenModuleRead(contents.as_ptr() as *mut i8, contents.len()) }
 }
 
+fn get_function(name: &str, module: BinaryenModuleRef) -> BinaryenFunctionRef {
+    unsafe {
+        let name = CString::new(name).unwrap();
+        BinaryenGetFunction(module, name.as_ptr())
+    }
+}
+
+fn get_function_name(func: BinaryenFunctionRef) -> String {
+    unsafe {
+        let name = BinaryenFunctionGetName(func);
+        CStr::from_ptr(name).to_string_lossy().into_owned()
+    }
+}
+
 fn main() {
     unsafe {
         let module = read_module("config.wasm");
 
         BinaryenModulePrint(module);
+
+        let model_path_func = get_function("inf:wasi/config#get-model-path", module);
+        let model_path_name = get_function_name(model_path_func);
+        println!("module_path_func: name: {:?}", model_path_name);
+        let body = BinaryenFunctionGetBody(model_path_func);
+        BinaryenExpressionPrint(body);
 
         BinaryenModuleDispose(module);
     }
