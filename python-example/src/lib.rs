a8378c1fe3e3aae0ffd393c846991b956e20834c
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-04-02 14:54:31 +0200

    src: add example of using emoji package
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- python-example/src/lib.rs
+++ python-example/src/lib.rs
@@ -1,4 +1,6 @@
-use numpy::PyArray1;
+//use numpy::ndarray::array;
+//use numpy::pyo3::Python;
+//use numpy::PyArray1;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
 
@@ -7,10 +9,10 @@ pub fn print() {
     pyo3::prepare_freethreaded_python();
 
     Python::with_gil(|py| {
-        println!("Import numpy...");
-        //let np = py.import("numpy").unwrap();
-        //let locals = [("np", np)].into_py_dict(py);
-        //let locals = [];
+        println!("Import emoji...");
+        let em = py.import("emoji").unwrap();
+        let locals = [("em", em)].into_py_dict(py);
+
         let builtins = PyModule::import(py, "builtins").unwrap();
 
         let some_code = PyModule::from_code(
@@ -25,10 +27,9 @@ def something(input):
         .unwrap();
         some_code.getattr("something").unwrap().call1(("bajja",));
 
-        let python_code = r#"print('Printing from Python!')"#;
-        println!("Going to eval the following Python code:  {}", python_code);
-        //let result = py.eval(python_code, Some(locals), None);
-        let result = py.eval(python_code, None, None);
+        let python_code = r#"print(em.emojize('Python is :thumbs_up:'))"#;
+        //println!("{}", python_code);
+        let result = py.eval(python_code, Some(locals), None);
         match result {
             Ok(_) => println!("Python code executed successfully!"),
             Err(e) => println!("Python code execution failed! {:#?}", e),
@@ -36,5 +37,8 @@ def something(input):
 
         let pm = py.import("time").unwrap();
         println!("{:?}", pm.getattr("ctime").unwrap().call0());
+
+        let pm = py.import("site").unwrap();
+        println!("{:?}", pm.getattr("getusersitepackages").unwrap().call0());
     });
 }
